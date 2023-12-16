ef8c6350d2d3cbe10c1cd0b030520dfa924e60af
Christos <kristocopani@gmail.com>
2023-12-16 17:44:43 +0200

    asfasfa

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 reflector_init() {
     reflector_install_status="pacman -Qs reflector"
-    if [ -n "$reflector_install_status" ]; then
+    if [ -ne "$reflector_install_status" ]; then
         echo "${GREEN}Reflector is installed"
         echo "${YELLOW}Configuring Reflector"
         sudo reflector --verbose -c AT -c BY -c BE -c BA -c BG -c DK -c FI -c FR -c GE -c DE -c GR -c IE -c LU -c MC -c NL -c NO -c ES -c SE -c CH -c GB --protocol score --sort score --latest 10 --download-timeout 5 --save /etc/pacman.d/mirrorlist >/dev/null
@@ -9,7 +9,7 @@ reflector_init() {
         echo "${RED}Reflector is not installed"
         sudo pacman -S reflector --noconfirm >/dev/null
         reflector_install_status="pacman -Qs reflector"
-        if [ -n "$reflector_install_status" ]; then
+        if [ -ne "$reflector_install_status" ]; then
             echo "${GREEN}Reflector installed successfully"
             echo "${YELLOW}Configuring Reflector"
             sudo reflector --verbose -c AT -c BY -c BE -c BA -c BG -c DK -c FI -c FR -c GE -c DE -c GR -c IE -c LU -c MC -c NL -c NO -c ES -c SE -c CH -c GB --protocol score --sort score --latest 10 --download-timeout 5 --save /etc/pacman.d/mirrorlist >/dev/null
@@ -29,14 +29,14 @@ gitconfig_init() {
 }
 git_init() {
     git_install_status="pacman -Qs git"
-    if [ -n "$git_install_status" ]; then
+    if [ -ne "$git_install_status" ]; then
         echo "${GREEN}Git is installed"
         gitconfig_init
     else
         echo "${RED}Git is not installed"
         echo "${YELLOW}Installing Git"
         sudo pacman -S git --noconfirm >/dev/null
-        if [ -n "$git_install_status" ]; then
+        if [ -ne "$git_install_status" ]; then
             echo "${GREEN}Git installed successfully"
             echo "${YELLOW}Configuring Git"
             gitconfig_init
